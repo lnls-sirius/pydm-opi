87c8605a8eafd5d7e9c49c43b5a240ac04cb3288
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-20 19:26:31 -0300

    Removed useless commands when launching the opi.

--- launch.sh
+++ launch.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-function cleanup {
-    deactivate
-    echo "  Bye !  "
-}
-
-#source .virtualenv/bin/activate
-trap cleanup EXIT
-
 export PYTHONPATH=${PWD}
-export HOME=${PWD}
 
 # PV Gateway Address
-export EPICS_CA_ADDR_LIST=10.0.6.57
 export EPICS_CA_AUTO_ADDR_LIST=YES
 
 # PYDM expects the following language setup
