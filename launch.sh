779b464d5dc5fe2d80559da8f50c482334e57892
carneirofc <claudiofcarneiro@hotmail.com>
2019-04-23 11:53:15 -0300

    launch: removed unused environment variables.

--- launch.sh
+++ launch.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=${PWD}
-
-# PV Gateway Address
-export EPICS_CA_AUTO_ADDR_LIST=YES
-# PYDM expects the following language setup
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-
-OPTS="--hide-nav-bar --hide-menu-bar --hide-status-bar"
-pydm $OPTS src/launcher.py
+OPT="--hide-nav-bar --hide-menu-bar --hide-status-bar"
+pydm $OPT src/launcher.py
