2e5fe27187deda3962ff24cb954dee5e2b4fa4f2
carneirofc <claudiofcarneiro@hotmail.com>
2019-05-29 11:32:41 -0300

    update launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=${PWD}
+ping -c 1 10.0.38.42
+if [ $?=0 ]; then
+    rm Redes\ e\ Beaglebones.xlsx*
+    wget http://10.0.38.42/streamdevice-ioc/Redes%20e%20Beaglebones.xlsx
+fi
+xhost +local:docker
 OPT="--hide-nav-bar --hide-menu-bar --hide-status-bar"
 pydm $OPT src/launcher.py
