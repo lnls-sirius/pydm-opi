8bd5413299a0a6c789a22e969ea9475a4be242f9
anacso17 <anacso1777@gmail.com>
2019-10-03 13:30:39 -0300

    Move .ui files to their respective folders in src. Fix pctrl symlink.

--- src/paths.py
+++ src/paths.py
@@ -22,11 +22,11 @@ with open(get_abs_path('../css/table-alarm.qss')) as f:
 
 
 LAUNCH_WINDOW = "launcher/launcher.py"
-LAUNCH_WINDOW_UI = "../ui/launcher.ui"
+LAUNCH_WINDOW_UI = "launcher/ui/launcher.ui"
 
-OVERVIEW_UI = '../ui/overview.ui'
+OVERVIEW_UI = 'utils/ui/overview.ui'
 
-PCTRL_MAIN = '../ui/pctrl/main.py'
+PCTRL_MAIN = 'pctrl/main.py'
 
 EPP_MAIN = '../ui/spixconv/launch_ui_main_window.py'
 
@@ -34,7 +34,7 @@ EPP_MAIN = '../ui/spixconv/launch_ui_main_window.py'
 #                                   MBTemp                                    #
 ###############################################################################
 MBTEMP_MAIN = 'mbtemp/main.py'
-MBTEMP_MAIN_UI = '../ui/mbtemp/main.ui'
+MBTEMP_MAIN_UI = 'mbtemp/ui/main.ui'
 
 ###############################################################################
 #                                  Agilent                                    #
@@ -42,16 +42,16 @@ MBTEMP_MAIN_UI = '../ui/mbtemp/main.ui'
 AGILENT_OVERVIEW = 'agilent4uhv/overview.py'
 
 AGILENT_MAIN = "agilent4uhv/main.py"
-AGILENT_MAIN_UI = "../ui/agilent4uhv/main.ui"
+AGILENT_MAIN_UI = "agilent4uhv/ui/main.ui"
 
 AGILENT_DEVICE_MAIN = "agilent4uhv/device_main.py"
-AGILENT_DEVICE_MAIN_UI = "../ui/agilent4uhv/device_main.ui"
+AGILENT_DEVICE_MAIN_UI = "agilent4uhv/ui/device_main.ui"
 
 # AGILENT_DEVICE = "agilent4uhv/device.py"
-AGILENT_DEVICE_UI = "../ui/agilent4uhv/device.ui"
+AGILENT_DEVICE_UI = "agilent4uhv/ui/device.ui"
 
 # AGILENT_CHANNEL = "agilent4uhv/channel.py"
-AGILENT_CHANNEL_UI = "../ui/agilent4uhv/channel.ui"
+AGILENT_CHANNEL_UI = "agilent4uhv/ui/channel.ui"
 
 ###############################################################################
 #                                      MKS                                    #
@@ -59,45 +59,45 @@ AGILENT_CHANNEL_UI = "../ui/agilent4uhv/channel.ui"
 MKS_OVERVIEW = 'mks937b/overview.py'
 
 MKS_MAIN = "mks937b/table.py"
-MKS_MAIN_UI = "../ui/mks937b/table.ui"
+MKS_MAIN_UI = "mks937b/ui/table.ui"
 
 TABLE = 'mks937b/table.py'
-TABLE_UI = '../ui/mks937b/table.ui'
+TABLE_UI = 'mks937b/ui/table.ui'
 
 # STORAGE_RING = 'mks937b/storage_ring.py'
-STORAGE_RING_UI = '../ui/mks937b/storage_ring.ui'
+STORAGE_RING_UI = 'mks937b/ui/storage_ring.ui'
 
 # BOOSTER = 'mks937b/booster.py'
-BOOSTER_UI = '../ui/mks937b/booster.ui'
+BOOSTER_UI = 'mks937b/ui/booster.ui'
 
 # BTS = 'mks937b/bts.py'
-BTS_UI = '../ui/mks937b/bts.ui'
+BTS_UI = 'mks937b/ui/bts.ui'
 
 # LTB = 'mks937b/ltb.py'
-LTB_UI = '../ui/mks937b/ltb.ui'
+LTB_UI = 'mks937b/ui/ltb.ui'
 
-NONE_UI = '../ui/mks937b/none.ui'
+NONE_UI = 'mks937b/ui/none.ui'
 
 DEVICE_PREVIEW = 'mks937b/device_preview.py'
-DEVICE_PREVIEW_UI = '../ui/mks937b/device_preview.ui'
+DEVICE_PREVIEW_UI = 'mks937b/ui/device_preview.ui'
 
 # CC = 'mks937b/cc.py'
-CC_UI = '../ui/mks937b/cc.ui'
+CC_UI = 'mks937b/ui/cc.ui'
 
 # PR = 'mks937b/pirani.py'
-PR_UI = '../ui/mks937b/pirani.ui'
+PR_UI = 'mks937b/ui/pirani.ui'
 
 PRESSURE = 'mks937b/pressure.py'
-PRESSURE_UI = '../ui/mks937b/pressure.ui'
+PRESSURE_UI = 'mks937b/ui/pressure.ui'
 
 SETTINGS = 'mks937b/settings.py'
-SETTINGS_UI = '../ui/mks937b/settings.ui'
+SETTINGS_UI = 'mks937b/ui/settings.ui'
 
 # INFO = 'mks937b/info.py'
-INFO_UI = '../ui/mks937b/info.ui'
+INFO_UI = 'mks937b/ui/info.ui'
 
 DEVICE_MENU = 'mks937b/device_menu.py'
-DEVICE_MENU_UI = '../ui/mks937b/device_menu.ui'
+DEVICE_MENU_UI = 'mks937b/ui/device_menu.ui'
 
 # IOC_MAN = 'mks937b/ioc_man.py'
-IOC_MAN_UI = '../ui/mks937b/ioc_man.ui'
+IOC_MAN_UI = 'mks937b/ui/ioc_man.ui'
