3cfeb6fe8a52ded9bae0a2efbd16aa01e40b4b71
carneirofc <claudiofcarneiro@hotmail.com>
2019-10-08 10:18:25 -0300

    Remove white space

--- scripts/sirius-hla-as-ap-pctrl.py
+++ scripts/sirius-hla-as-ap-pctrl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import subprocess
-from siriushlacon.pctrl.consts import PCTRL_MAIN 
+from siriushlacon.pctrl.consts import PCTRL_MAIN
 
 subprocess.Popen('pydm --hide-nav-bar '+ PCTRL_MAIN, shell=True)
