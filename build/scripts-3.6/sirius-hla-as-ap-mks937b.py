36b7c746af22410efe814077fbe71225c008ea0e
ximenes <xresende@gmail.com>
2020-02-10 12:08:27 -0300

    Delete files in build

--- build/scripts-3.6/sirius-hla-as-ap-mks937b.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-import os
-from siriushlacon.mks937b.consts import MKS_MAIN
-
-os.environ['PYDM_DEFAULT_PROTOCOL'] = 'ca://'
-
-subprocess.Popen('pydm --hide-nav-bar '+ MKS_MAIN, shell=True)
--- build/scripts-3.6/sirius-hla-as-ap-pctrl.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-import os
-from siriushlacon.pctrl.consts import PCTRL_MAIN
-
-os.environ['PYDM_DEFAULT_PROTOCOL'] = 'ca://'
-subprocess.Popen('pydm --hide-nav-bar '+ PCTRL_MAIN, shell=True)
