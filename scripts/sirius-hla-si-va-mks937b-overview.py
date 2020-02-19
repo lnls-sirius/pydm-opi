40268ed16963e337ff697ef0f1a5660df97946c7
anacso17 <anacso1777@gmail.com>
2020-02-19 11:10:01 -0300

    scripts: add option to run MKS graph windows

--- scripts/sirius-hla-si-va-mks937b-overview.py
+++ scripts/sirius-hla-si-va-mks937b-overview.py
@@ -1,10 +1,21 @@
-#!/usr/local/env python3
-import subprocess
+#!/usr/bin/env python3
 import os
-from siriushlacon.mks937b.consts import MKS_OVERVIEW
+import subprocess
+import argparse as _argparse
+from siriushlacon.mks937b.consts import MKS_GRAPH, MKS_OVERVIEW
 
+parser = _argparse.ArgumentParser(description="Run SI MKS Overview.")
+parser.add_argument('-graph', action='store_true')
+args = parser.parse_args()
 
 os.environ['PYDM_DEFAULT_PROTOCOL'] = 'ca://'
-macros = '\'{"device": "MKS", "TYPE": "SI", "TITLE": "MKS 937b - SI"}\''
 
-subprocess.Popen('pydm --hide-nav-bar -m '+macros+' '+MKS_OVERVIEW, shell=True)
+if args.graph:
+    f = MKS_GRAPH
+    macros = '\'{"TYPE": "SI"}\''
+else:
+    f = MKS_OVERVIEW
+    macros = '\'{"device": "MKS", "TYPE": "SI", "TITLE": "MKS 937b - SI"}\''
+
+
+subprocess.Popen('pydm --hide-nav-bar -m '+macros+' '+f, shell=True)
