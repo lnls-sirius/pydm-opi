8b78798b2b9ca6a7fb33cc8d886e13db94e1ff90
anacso17 <anacso1777@gmail.com>
2020-06-23 18:54:09 -0300

    Fix MKS overview scripts

--- scripts/sirius-hla-si-va-mks937b-overview.py
+++ scripts/sirius-hla-si-va-mks937b-overview.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-import argparse as _argparse
-from siriushlacon.mks937b.consts import MKS_GRAPH, MKS_OVERVIEW
+from siriushlacon.mks937b.consts import MKS_OVERVIEW
 
-parser = _argparse.ArgumentParser(description="Run SI MKS Overview.")
-parser.add_argument("-graph", action="store_true")
-args = parser.parse_args()
 
 os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
 
-if args.graph:
-    f = MKS_GRAPH
-    macros = '\'{"TYPE": "SI"}\''
-else:
-    f = MKS_OVERVIEW
-    macros = '\'{"device": "MKS", "TYPE": "SI", "TITLE": "MKS 937b - SI"}\''
-
-
-subprocess.Popen("pydm --hide-nav-bar -m " + macros + " " + f, shell=True)
+macros = '\'{"device": "MKS", "TYPE": "SI", "TITLE": "MKS 937b - SI"}\''
+subprocess.Popen("pydm --hide-nav-bar -m " + macros +
+                 " " + MKS_OVERVIEW, shell=True)
