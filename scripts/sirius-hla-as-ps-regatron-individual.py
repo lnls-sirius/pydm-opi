d6884fb76d0a504a9445d1229acca90ac9fd5ca2
carneirofc <claudiofcarneiro@hotmail.com>
2020-08-18 16:39:42 -0300

    Fix Windows compatibility problems when passing macros

--- scripts/sirius-hla-as-ps-regatron-individual.py
+++ scripts/sirius-hla-as-ps-regatron-individual.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import argparse as _argparse
+import json
 from siriushlacon.regatron.consts import COMPLETE_MAIN
 
 os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
@@ -9,6 +10,4 @@ os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
 parser = _argparse.ArgumentParser(description="Run SI PS DCLink Interface.")
 parser.add_argument("-dev", "--device", type=str, default="")
 args = parser.parse_args()
-macros = '\'{"P": "' + args.device + "\"}'"
-
-subprocess.Popen("pydm --hide-nav-bar -m " + macros + " " + COMPLETE_MAIN, shell=True)
+subprocess.Popen('pydm --hide-nav-bar -m P={} {}'.format(args.device, COMPLETE_MAIN), shell=True)
