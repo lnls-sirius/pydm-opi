9576f70519d10d76f9a3cce52307523b11c2d59f
carneirofc <claudiofcarneiro@hotmail.com>
2020-10-05 14:42:09 -0300

    Relative to abs path

--- siriushlacon/beaglebones/consts.py
+++ siriushlacon/beaglebones/consts.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import pkg_resources
 
+
 def get_abs_path(filename):
     return pkg_resources.resource_filename(__name__, filename)
 
-BEAGLEBONES_MAIN = get_abs_path('main.py')
-BEAGLEBONES_MAIN_UI = get_abs_path('ui/main.ui')
 
-INFO_BBB_UI = get_abs_path('ui/infoBBB.ui')
-CHANGE_BBB_UI = get_abs_path('ui/configBBB.ui')
+BEAGLEBONES_MAIN = get_abs_path("main.py")
+BEAGLEBONES_MAIN_UI = get_abs_path("ui/main.ui")
+
+INFO_BBB_UI = get_abs_path("ui/infoBBB.ui")
+CHANGE_BBB_UI = get_abs_path("ui/configBBB.ui")
