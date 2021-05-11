348333b68975ed82d8c5d7aef6bca09e15dfab5e
carneirofc <claudiofcarneiro@hotmail.com>
2021-05-11 15:15:17 -0300

    VBC EPICS settings. Squared byte indicator

--- scripts/sirius-hla-as-va-vbc.py
+++ scripts/sirius-hla-as-va-vbc.py
@@ -5,6 +5,14 @@ from siriushlacon.vbc.consts import MAIN_WINDOW_PY as VBC_MAIN_WINDOW_PY
 
 os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
 
+os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "YES"
+
+if "EPICS_CA_ADDR_LIST" not in os.environ:
+    os.environ["EPICS_CA_ADDR_LIST"] = "10.128.40.2:5068"
+else:
+    os.environ["EPICS_CA_ADDR_LIST"] = "10.128.40.2:5068" + os.environ["EPICS_CA_ADDR_LIST"]
+
+
 subprocess.Popen(
     f"pydm --hide-nav-bar {VBC_MAIN_WINDOW_PY}", shell=True
 )
