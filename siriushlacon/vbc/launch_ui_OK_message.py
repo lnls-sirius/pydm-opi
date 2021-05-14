10889e5901b626417a089d7e4e8b9e9e1a8394ff
carneirofc <claudiofcarneiro@hotmail.com>
2021-05-14 13:06:41 -0300

    vbc/ fix OK message

--- siriushlacon/vbc/launch_ui_OK_message.py
+++ siriushlacon/vbc/launch_ui_OK_message.py
@@ -3,15 +3,26 @@ import sys
 from qtpy.QtGui import QPixmap
 from pydm import Display
 from siriushlacon.vbc.consts import OK_MESSAGE_UI, CLEAN_STATUS_SCRIPT, CHECK_IMG
+from siriushlacon.vbc.command_runner import ShellCommandRunner
 
 
 class DeviceMenu(Display):
-    def __init__(self, parent=None, args=[], macros=None):
+    def __init__(self, parent=None, args=None, macros=None):
         super(DeviceMenu, self).__init__(
             parent=parent, args=args, macros=macros, ui_filename=OK_MESSAGE_UI
         )
         self.label_2.setPixmap(QPixmap(CHECK_IMG))
 
-        finished = sys.argv[5]
+        if len(sys.argv) < 6:
+            raise RuntimeError(f"Invalid arguments {sys.argv}")
 
-        self.Shell_clean_PVs.commands = [f"python {CLEAN_STATUS_SCRIPT} {finished}"]
+        ioc_prefix = sys.argv[5]
+        finished = sys.argv[6]
+
+        self.CleanStatusCommand = ShellCommandRunner(
+            command=f"python {CLEAN_STATUS_SCRIPT} {ioc_prefix} {finished}"
+        )
+
+        self.pushButton.clicked(
+            lambda _, *_args, **_kwargs: self.CleanStatusCommand.execute_command()
+        )
